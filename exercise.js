676d6501922a54f4bb7246ad585352425924620e
LuthfiAlFinnegan <gatesluthfi@gmail.ac.id>
2023-08-31 09:45:47 +0700

    first commit

---
+++ exercise.js
@@ -0,0 +1,87 @@
+1; //
+// // problem = mencari luas persegi panjang
+// const rectangleLength = 10;
+// const rectangleWidth = 5;
+// const rectangleArea = rectangleLength * rectangleWidth;
+
+// console.log(`Rumus luas persegi panjang adalah L = P X L
+// P = ${rectangleLength}
+// L = ${rectangleWidth} `);
+// console.log(`${rectangleLength} * ${rectangleWidth} = ${rectangleArea}`);
+
+2; //
+// //  problem = mencari keliling persegi panjang
+// // K = 2 ( p + l) = Rumus keliling persegi panjang
+
+// const periLength = 5; // panjang
+// const periWidth = 7; // lebar
+// const perimeterArea = 2 * (periLength + periWidth); // eksekusi rumus
+
+// console.log(perimeterArea);
+
+3; //
+
+// const radius = 7;
+// let diameter = 2 * radius;
+// let circumference = Math.PI * diameter;
+// let area = Math.PI * radius * radius;
+
+// console.log(diameter);
+// console.log(circumference);
+// console.log(area);
+
+4; //
+// // problem : mencari sudut segitiga jika dua sudut sudah diberikan nilai
+// // rumus : 180 - (sudut1 + sudut@)
+
+// let sudut1 = 15;
+// let sudut2 = 30;
+// let sudut3 = 180 - (sudut1 + sudut2); // 180 adalah jumlah keseluruhan sudut
+
+// console.log(sudut3);
+
+5; //
+// let tanggal1 = new Date();
+// let tanggal2 = new Date("2024-01-01");
+
+// let perbedaan = tanggal2.getTime() - tanggal1.getTime();
+
+// let perbedaanHari = Math.floor(perbedaan / (1000 * 60 * 60 * 24));
+// console.log(perbedaanHari);
+
+6; //
+
+// let days = 400;
+// let daysOfYear = 365
+// let daysOfMonth = 30
+
+// let tahun = Math.floor(days / daysOfYear);
+// let sisa = days % daysOfYear;
+// let bulan = Math.floor(sisa / daysOfMonth);
+// let hari = sisa % daysOfMonth;
+
+// console.log(tahun);
+// console.log(bulan);
+// console.log(hari);
+
+// // membulatkan number
+// let num = 20.46;
+
+// console.log(Math.round(num)); // membulatkan dengan aturan matematika
+// console.log(Math.floor(num)); // membulatkan ke bawah
+// console.log(Math.ceil(num)); // membulatkan ke atas
+
+// let person = {
+//   nama: "Luthfi",
+//   umur: "18",
+// };
+
+// let newPerson = person;
+
+// console.log(person);
+// console.log(newPerson);
+
+// newPerson.nama = "bro";
+
+// console.log(person);
+// console.log(newPerson);
