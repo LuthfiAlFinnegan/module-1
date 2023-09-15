278ee0756e2e8643dcbe93dcbcc4abd00c4c3ec8
LuthfiAlFinnegan <gatesluthfi@gmail.ac.id>
2023-09-15 21:45:56 +0700

    day-14 calculator

--- exercise.js
+++ exercise.js
@@ -1,29 +1,3 @@
-function penjumlahan(a, b) {
-  return a + b;
-}
-function pengurangan(a, b) {
-  return a - b;
-}
-function pembagian(a, b) {
-  if (b == 0) {
-    return "Cannot divide by zero!";
-  } else {
-    return a / b;
-  }
-}
-function perkalian(a, b) {
-  return a * b;
-}
-function pangkat(a) {
-  return a * a;
-}
-function akarPangkat(a) {
-  return Math.sqrt(a);
-}
-function akarPangkat3(a) {
-  return Math.cbrt(a);
-}
-
 function calculator() {
   let value = prompt(`Pilih Menu:
 1.Penjumlahan
@@ -33,40 +7,88 @@ function calculator() {
 5.Pangkat 2
 6.Akar Pangkat 2
 7.Akar Pangkat 3`);
-  let num1 = parseFloat(prompt("Enter the first number:"));
-  let num2 = parseFloat(prompt("Enter the second number:"));
+  function penjumlahan(a, b) {
+    return parseFloat(a) + parseFloat(b);
+  }
+  function pengurangan(a, b) {
+    return a - b;
+  }
+  function pembagian(a, b) {
+    if (b == 0) {
+      return "Tidak bisa dibagi dengan nol!";
+    } else {
+      return a / b;
+    }
+  }
+  function perkalian(a, b) {
+    return a * b;
+  }
+  function pangkat(a) {
+    return a * a;
+  }
+  function akarPangkat(a) {
+    return Math.sqrt(a);
+  }
+  function akarPangkat3(a) {
+    return Math.cbrt(a);
+  }
+
+  alert("SELAMAT DATANG DI CALCULATOR TERCINTA");
+
+  let num1;
+  let num2;
+
+  if (value == 0) {
+    alert("PILIH YANG ADA DI MENU BANG!!!!");
+    calculator();
+  } else if (value > 0 && value < 5) {
+    num1 = prompt("Masukan angka pertama:");
+    num2 = prompt("Masukan angka kedua:");
+  } else {
+    num1 = prompt("Masukan angka:");
+  }
 
   let result;
 
   switch (value) {
-    case "Penjumlahan":
+    case "1":
       result = penjumlahan(num1, num2);
+      alert(`hasil penjumlahan dari ${num1} + ${num2} = ${result}`);
       break;
-    case "Pengurangan":
+    case "2":
       result = pengurangan(num1, num2);
+      alert(`hasil pengurangan dari ${num1} - ${num2} = ${result}`);
       break;
-    case "Pembagian":
+    case "3":
       result = pembagian(num1, num2);
+      alert(`hasil pembagian dari ${num1} / ${num2} = ${result}`);
       break;
-    case "Perkalian":
+    case "4":
       result = perkalian(num1, num2);
+      alert(`hasil perkalian dari ${num1} X ${num2} = ${result}`);
       break;
-    case "Pangkat 2":
+    case "5":
       result = pangkat(num1, num2);
+      alert(`hasil pangkat 2 dari ${num1} = ${result}`);
       break;
-    case "Akar Pangkat 2":
+    case "6":
       result = akarPangkat(num1, num2);
+      alert(`hasil akar pangkat 3 dari ${num1} = ${result}`);
       break;
-    case "Akar Pangkat 3":
+    case "7":
       result = akarPangkat3(num1, num2);
+      alert(`hasil akar pangkat 3 dari ${num1} = ${result}`);
       break;
-
     default:
       result = "PILIH YANG ADA DI MENU BANG! GAUSAH ANEH ANEH DEH AH.";
       break;
   }
-  alert(`result: ${result}`);
+  const valConfirm = confirm("Coba lagi ga bang");
+  if (valConfirm == true) {
+    calculator();
+  } else {
+    alert("Thankyou sudah pakai saya puhhh");
+  }
 }
 
 calculator();
-
