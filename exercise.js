f0aa9ab081148db684f7548b23a51031013afb90
LuthfiAlFinnegan <gatesluthfi@gmail.ac.id>
2023-09-19 19:33:05 +0700

    day-15 exercise

--- exercise.js
+++ exercise.js
@@ -1,94 +1,173 @@
-function calculator() {
-  let value = prompt(`Pilih Menu:
-1.Penjumlahan
-2.Pengurangan
-3.Pembagian
-4.Perkalian
-5.Pangkat 2
-6.Akar Pangkat 2
-7.Akar Pangkat 3`);
-  function penjumlahan(a, b) {
-    return parseFloat(a) + parseFloat(b);
-  }
-  function pengurangan(a, b) {
-    return a - b;
-  }
-  function pembagian(a, b) {
-    if (b == 0) {
-      return "Tidak bisa dibagi dengan nol!";
-    } else {
-      return a / b;
-    }
-  }
-  function perkalian(a, b) {
-    return a * b;
-  }
-  function pangkat(a) {
-    return a * a;
-  }
-  function akarPangkat(a) {
-    return Math.sqrt(a);
-  }
-  function akarPangkat3(a) {
-    return Math.cbrt(a);
-  }
+// 1. weekend //
+// const weekend = (date) => {
+//   let dayOfWeek = date.getDay();
 
-  alert("SELAMAT DATANG DI CALCULATOR TERCINTA");
+//   return dayOfWeek === 0 || dayOfWeek === 6;
+// };
 
-  let num1;
-  let num2;
+// const checkDate = new Date(2023, 8, 16);
 
-  if (value == 0) {
-    alert("PILIH YANG ADA DI MENU BANG!!!!");
-    calculator();
-  } else if (value > 0 && value < 5) {
-    num1 = prompt("Masukan angka pertama:");
-    num2 = prompt("Masukan angka kedua:");
-  } else {
-    num1 = prompt("Masukan angka:");
-  }
+// if (weekend(checkDate)) {
+//   console.log(`is weekend`);
+// } else {
+//   console.log(`is not weekend`);
+// }
 
-  let result;
+// 2. gcd //
+// const gcd = (x, y) => {
+//   if (typeof x !== "number" || typeof y !== "number") return false;
+//   x = Math.abs(x);
+//   y = Math.abs(y);
+//   while (y) {
+//     let t = y;
+//     y = x % t;
+//     x = t;
+//   }
+//   return x;
+// };
+// console.log(gcd(48, 66));
 
-  switch (value) {
-    case "1":
-      result = penjumlahan(num1, num2);
-      alert(`hasil penjumlahan dari ${num1} + ${num2} = ${result}`);
-      break;
-    case "2":
-      result = pengurangan(num1, num2);
-      alert(`hasil pengurangan dari ${num1} - ${num2} = ${result}`);
-      break;
-    case "3":
-      result = pembagian(num1, num2);
-      alert(`hasil pembagian dari ${num1} / ${num2} = ${result}`);
-      break;
-    case "4":
-      result = perkalian(num1, num2);
-      alert(`hasil perkalian dari ${num1} X ${num2} = ${result}`);
-      break;
-    case "5":
-      result = pangkat(num1, num2);
-      alert(`hasil pangkat 2 dari ${num1} = ${result}`);
-      break;
-    case "6":
-      result = akarPangkat(num1, num2);
-      alert(`hasil akar pangkat 3 dari ${num1} = ${result}`);
-      break;
-    case "7":
-      result = akarPangkat3(num1, num2);
-      alert(`hasil akar pangkat 3 dari ${num1} = ${result}`);
-      break;
-    default:
-      result = "PILIH YANG ADA DI MENU BANG! GAUSAH ANEH ANEH DEH AH.";
-      break;
-  }
-  const valConfirm = confirm("Coba lagi ga bang");
-  if (valConfirm == true) {
-    calculator();
-  } else {
-    alert("Thankyou sudah pakai saya puhhh");
-  }
-}
+// 3. lcm //
+// let x = 13;
+// let y = 48;
+// const lcm = (a, b) => {
+//   return (a * b) / gcd(x, y);
+// };
+// a = x; // 13
+// b = y; // 48
+// let result;
+// do {
+//   result = lcm(a, b);
+//   a = result;
+//   b = Math.max(x, y);
+// } while (a % b !== 0);
+// console.log(lcm(x, y));
 
-calculator();
+// 4. most common character from a string //
+// const findCommon = (str) => {
+//   const obj = {};
+//   let charMax = "";
+//   let count = 0;
+
+//   for (let char of str) {
+//     if (obj[char]) {
+//       obj[char]++;
+//     } else {
+//       obj[char] = 1;
+//     }
+
+//     for (let char in obj) {
+//       if (obj[char] > count) {
+//         count = obj[char];
+//         charMax = char;
+//       }
+//     }
+//   }
+//   return charMax;
+// };
+// console.log(findCommon("asdasdasdaaaaa"));
+
+// 5 //
+// const alphabet = (str) => {
+//   return str.split("").sort().join("");
+// };
+// console.log(alphabet("aabbccaaa"));
+
+// const alphabet = (str) => {
+//   let str2 = str.split("");
+//   let min;
+//   for (let i = 0; i < str2.length; i++) {
+//     min = i;
+
+//     for (let j = i + 1; j < str2.length; j++) {
+//       if (str2[j] < str2[i]) min = j;
+//     }
+//     if (min !== i) [str2[i], str2[min]] = [str2[min], str2[i]];
+//   }
+//   return str2.join("");
+// };
+// console.log(alphabet("aabbccaa"));
+
+// 6 //
+// let date = [
+//     new Date("2024-09-18"),
+//     new Date("2024-09-19"),
+//     new Date("2024-09-17"),
+// ];
+
+// const minim = (date) => {
+//   let minimum = new Date(Math.min.apply(null, date));
+//   let str = minimum.toISOString().split("T")[0]
+//   console.log(str);
+// };
+// minim(date);
+
+// 7 //
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// const sum = (arr) => {
+//   let output = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       output += arr[i][j];
+//     }
+//   }
+//   return output;
+// };
+// console.log(sum(arr));
+
+// 8 //
+// const prefix = (arr) => {
+//   let size = arr.length;
+
+//   if (size == 0) return "";
+
+//   if (size == 1) return arr[0];
+
+//   arr.sort();
+
+//   let end = Math.min(arr[0].length, arr[size - 1].length);
+
+//   let i = 0;
+//   while (i < end && arr[0][i] == arr[size - 1][i]) {
+//     i++;
+//   }
+//   let pre = arr[0].substring(0, i);
+//   return pre;
+// };
+// console.log(prefix(["apple", "ape", "april", "apem"]));
+
+// 9 //
+// let arr = [null, "bang", 1, undefined, "halo", 13];
+// const copies = (arr) => {
+//   let mid = Math.ceil(arr.length / 2);
+//   let cop = arr.slice(0, mid);
+//   return cop;
+// };
+// console.log(copies(arr));
+
+// 10 //
+// const myFunc = (month, year) => new Date(year, month, 0).getDate();
+// console.log(myFunc(3, 2023));
+
+// 11 //
+// const findDay = () => {
+//   let now = new Date();
+//   let start = new Date(now.getFullYear(), 0, 0);
+//   let diff = now - start;
+//   let oneDay = 1000 * 60 * 60 * 24;
+//   let day = Math.floor(diff / oneDay);
+//   return day;
+// };
+// console.log(findDay());
+
+// 12 //
+// const age = (birth) => {
+//   let min = new Date() - new Date(birth);
+//   let tahun = min / (1000 * 60 * 60 * 24);
+//   return Math.floor(tahun / 365);
+// };
+// console.log(age("2004-09-22"));
--- sum.spec.js
+++
@@ -1,6 +0,0 @@
-const { sum } = require("./sum");
-
-test("Testing sum function", () => {
-  expect(sum(10, 5)).toBe(15);
-  expect(sum(10, 10)).toBe(20);
-});
